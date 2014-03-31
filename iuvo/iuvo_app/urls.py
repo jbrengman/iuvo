26a20e20309e2c3154df165050bd7a57bff8b85e
Jordan Brengman <jordanbrengman@gmail.com>
2014-03-31 12:45:12 -0700

    Fill urls and create view methods

--- iuvo/iuvo_app/urls.py
+++ iuvo/iuvo_app/urls.py
@@ -3,5 +3,19 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'iuvo_app.views',
-    url(r'^$', 'home_view', name='home')
+    url(r'^$', 'home_view', name='home'),
+    url(r'^login/$', 'login_view', name='login'),
+    url(r'^register/$', 'register_view', name='register'),
+
+    url(r'^(\d+)/$', 'dashboard_view', name='dashboard'),
+
+    url(r'^(\d+)/events/$', 'events_list_view', name='events_list'),
+    url(r'^(\d+)/events/new/$', 'create_event_view', name='create_event'),
+    url(r'^(\d+)/events/(\d+)/$', 'view_event_view', name='view_event'),
+    url(r'^(\d+)/events/(\d+)/edit/$', 'edit_event_view', name='edit_event'),
+
+    url(r'^(\d+)/contacts/$', 'contacts_list_view', name='contacts_list'),
+    url(r'^(\d+)/contacts/new/$', 'create_contact_view', name='create_contact'),
+    url(r'^(\d+)/contacts/(\d+)/$', 'view_contact_view', name='view_contact'),
+    url(r'^(\d+)/contacts/(\d+)/edit/$', 'edit_contact_view', name='edit_contact'),
 )
--- iuvo/iuvo_app/views.py
+++ iuvo/iuvo_app/views.py
@@ -8,3 +8,39 @@ from iuvo_app.models import User, Event, Contact
 def home_view(request):
     context = {}
     return render(request, 'placeholder.html', context)
+
+
+def login_view(request):
+    pass
+
+
+def register_view(request):
+    pass
+
+
+def dashboard_view(request, user_id):
+    pass
+
+
+def create_event_view(request):
+    pass
+
+
+def edit_event_view(request):
+    pass
+
+
+def create_contact_view(request):
+    pass
+
+
+def edit_contact_view(request):
+    pass
+
+
+def check_in_view(request):
+    pass
+
+
+def about_view(request):
+    pass

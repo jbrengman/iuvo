b2c76ed8ea354c669474abf05e284068ac543022
Jordan Brengman <jordanbrengman@gmail.com>
2014-03-31 10:33:50 -0700

    Install south and psycopg2,  create iuvo_app

--- iuvo/iuvo/settings.py
+++ iuvo/iuvo/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'iuvo_app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +59,8 @@ WSGI_APPLICATION = 'iuvo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'iuvodb',
     }
 }
 

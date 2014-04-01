106c29495dbccca9c588b4c999239d4dd3d23bfa
James White <jwhite007@comcast.net>
2014-03-31 19:11:02 -0700

    added a test function in cron.py.  changed cron settings in settings.py to run the test function.  changed "url(r'^', include(iuvo_app.urls))" to "url(r'^', include('iuvo_app.urls'))" in project urls.py

--- iuvo/iuvo/urls.py
+++ iuvo/iuvo/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import iuvo_app.urls
+# import iuvo_app.urls
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(iuvo_app.urls))
+    url(r'^', include('iuvo_app.urls'))
 )
